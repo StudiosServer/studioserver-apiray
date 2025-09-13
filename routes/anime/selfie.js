21ea5268991e9a1f19bcbe056ab3a477a298d086
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:17:26 -0500

    Upload routes/anime/selfie.js

---
+++ routes/anime/selfie.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+router.get('/anime/selfie', async (req, res) => {
+    try {
+        // URL de la API que devuelve el JSON
+        const apiUrl = 'https://delirius-apiofc.vercel.app/anime/selfie';
+        const apiResponse = await axios.get(apiUrl);
+
+        // Extraer la URL de la imagen del JSON
+        const imageUrl = apiResponse.data.data.image;
+        
+        // Descargar la imagen
+        const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+        // Guardar la imagen en el directorio especificado
+        const imagePath = path.join(__dirname, 'tempdata/selfie.png');
+        fs.writeFileSync(imagePath, imageResponse.data);
+
+        // Enviar la imagen como respuesta
+        res.sendFile(imagePath);
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({ status : false });
+    }
+});
+
+module.exports = router;
