c0c07f243a1a91a7e834e7844a92d4f87c413362
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:11:34 -0500

    Add web/iaimage.js

---
+++ web/iaimage.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/iaimage', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'iaimage.html'));
+});
+
+module.exports = router;
