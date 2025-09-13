2d90c39e60b7c5caec5506f5f090f384c3e224f6
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:21:29 -0500

    Upload routes/tools/ibb.js

---
+++ routes/tools/ibb.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+// Ruta para obtener los datos de TikTok
+router.get('/tools/ibb', async (req, res) => {
+    const query = req.query.query;
+    const filename = req.query.filename;
+    if (!query) {
+        return res.status(400).json({ error: 'El parámetro de consulta "query=url image" es requerido' });
+    }
+    if (!filename) {
+        return res.status(400).json({ error: 'El parámetro de consulta "filename=my-file" es requerido' });
+    }
+
+    try {
+        const apiURL = `https://delirius-apiofc.vercel.app/tools/ibb?image=${encodeURIComponent(query)}&filename=${encodeURIComponent(filename)}`;
+        const response = await axios.get(apiURL);
+
+        res.json({
+            creator: 'Studio Server',
+            status: true,
+            result: response.data.data
+        });
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({ status: false });
+    }
+});
+
+module.exports = router;
