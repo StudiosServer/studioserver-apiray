9d0a78585c3ef90ea40a6e46ebf7827d9fc4d382
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:20:14 -0500

    Upload routes/search/googleimage.js

---
+++ routes/search/googleimage.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+// Ruta de búsqueda personalizada para imágenes de Google
+router.get('/search/gimage', async (req, res) => {
+    const query = req.query.query;
+
+    if (!query) {
+        return res.status(400).json({
+            creator: 'Studio Server',
+            status: false,
+            message: 'El parámetro de consulta "query" es requerido'
+        });
+    }
+
+    try {
+        const apiURL = `https://delirius-apiofc.vercel.app/search/gimage?query=${encodeURIComponent(query)}`;
+        const response = await axios.get(apiURL);
+
+        const formattedData = {
+            creator: 'Studio Server',
+            status: true,
+            data: {}
+        };
+        response.data.data.forEach(item => {
+            formattedData.data[item.origin.title] = item;
+        });
+
+        res.json(formattedData);
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({
+            creator: 'Studio Server',
+            status: false,
+            message: 'developing system'
+        });
+    }
+});
+
+module.exports = router;
