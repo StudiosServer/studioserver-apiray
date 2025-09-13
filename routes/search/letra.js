00d3b1cee85ba028d9129b708da1f5a35b4e411c
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:20:17 -0500

    Upload routes/search/letra.js

---
+++ routes/search/letra.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+// Ruta de búsqueda personalizada
+router.get('/search/letra', async (req, res) => {
+    const query = req.query.query;
+    if (!query) {
+        return res.status(400).json({ error: 'El parámetro de consulta "query" es requerido' });
+    }
+
+    try {
+        const apiURL = `https://delirius-apiofc.vercel.app/search/letra?query=${encodeURIComponent(query)}`;
+        const response = await axios.get(apiURL);
+
+        res.json(response.data);
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({ status : false });
+    }
+});
+
+module.exports = router;
