d0da46f62e23b94d7fab7f3ccd75ba8e0a7e8a3c
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:21:34 -0500

    Upload routes/tools/mojito.js

---
+++ routes/tools/mojito.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+// Ruta para obtener los datos de TikTok
+router.get('/tools/mojito', async (req, res) => {
+    const query = req.query.query;
+    if (!query) {
+        return res.status(400).json({ error: 'El parámetro de consulta "query=😂" es requerido' });
+    }
+
+    try {
+        const apiURL = `https://delirius-apiofc.vercel.app/tools/mojito?emoji=${encodeURIComponent(query)}`;
+        const response = await axios.get(apiURL);
+
+        res.json({
+            creator: 'Studio Server',
+            status: true,
+            result: response.data.result
+        });
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({ status: false });
+    }
+});
+
+module.exports = router;
