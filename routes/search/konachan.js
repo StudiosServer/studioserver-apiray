e3eca90ef58d31efd093c49bc5331feb6fa73c10
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:20:16 -0500

    Upload routes/search/konachan.js

---
+++ routes/search/konachan.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+// Ruta de búsqueda personalizada para Konachan
+router.get('/search/konachan', async (req, res) => {
+    const query = req.query.query;
+
+    if (!query) {
+        return res.status(400).json({
+            creator: 'Studio Server',
+            status: false,
+            message: 'El parámetro de consulta "q" es requerido'
+        });
+    }
+
+    try {
+        const apiURL = `https://delirius-apiofc.vercel.app/search/konachan?q=${encodeURIComponent(query)}`;
+        const response = await axios.get(apiURL);
+
+        const formattedData = response.data.data.map(item => item);
+
+        res.json({
+            creator: 'Studio Server',
+            status: true,
+            data: formattedData
+        });
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({
+            creator: 'Studio Server',
+            status: false,
+            message: 'developing system'
+        });
+    }
+});
+
+module.exports = router;
