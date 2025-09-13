41283e8c522be563e51da392a3c398041ff22bdc
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:11:14 -0500

    Add config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+module.exports = {
+    //search spotify
+    client_id: '94a8f2df6f494aa58094beef09f34fd9',
+    client_secret: 'b3ab0a8d944d4f478a9209703246416d',
+    //
+    //search youtube
+    youtube_api_key: 'AIzaSyBh3Kgu9ic5MzJauzVSWw-kRiEhWEb6jqU',
+    //
+    //ia gemini
+    gemini_api_key: 'AIzaSyAXY8Y_3Sw0bCrmdDVM4zfVkyGrNr-n8bs',
+    //
+    //cloudflare tunnel
+    tunnel_token: process.env.CLOUDFLARE_TUNNEL_TOKEN || 'eyJhIjoiOTMwM2JhYzI1ZDc4ZjI1MmUzMzAwMzgwOTk5MjFiMzUiLCJ0IjoiNTZiYTFjN2ItMGNiNi00ZjI0LThkZmItYzQwODUyZTliMGMwIiwicyI6Ik4ySmlZekE1TmpVdFlXWm1ZaTAwT1RZNExUa3pNREF0TURFMFpqSXpZamN4WW1KaiJ9'
+};
