b7cefdaa63dd682d03a87c620a3e88c8c32e06f8
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:11:19 -0500

    Add routes/anime/animeinfo.js

---
+++ routes/anime/animeinfo.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+// Ruta para la API de búsqueda de anime
+router.get('/anime/animeinfo', async (req, res) => {
+    const query = req.query.query;
+
+    if (!query) {
+        return res.status(400).json({
+            creator: 'Studio Server',
+            status: false,
+            message: 'El parámetro de consulta "query" es requerido'
+        });
+    }
+
+    try {
+        const apiURL = `https://delirius-apiofc.vercel.app/anime/animeinfo?query=${encodeURIComponent(query)}`;
+        const response = await axios.get(apiURL);
+
+        const modifiedResponse = {
+            creator: 'Studio Server',
+            status: response.data.status,
+            data: response.data.data
+        };
+
+        res.json(modifiedResponse);
+        
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({
+            creator: 'Studio Server',
+            status: false,
+            message: 'developing system'
+        });
+    }
+});
+
+module.exports = router;
