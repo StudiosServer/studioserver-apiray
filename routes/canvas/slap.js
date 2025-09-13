20e634afd19f973a3daee128c029f2a67c72c124
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:18:23 -0500

    Upload routes/canvas/slap.js

---
+++ routes/canvas/slap.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+// Ruta para obtener los datos de TikTok
+router.get('/canvas/slap', async (req, res) => {
+    const query1 = req.query.query1;
+    const query2 = req.query.query2;
+    if (!query1) {
+        return res.status(400).json({ error: 'El parámetro de consulta "query1=url image" ejemplo requerido' });
+    }
+    if (!query2) {
+        return res.status(400).json({ error: 'El parámetro de consulta "query2=url image" ejemplo requerido' });
+    }
+
+    try {
+        const apiURL = `https://delirius-apiofc.vercel.app/canvas/slap?url1=${encodeURIComponent(query1)}&url2=${encodeURIComponent(query2)}`;
+        const response = await axios.get(apiURL, { responseType: 'arraybuffer' });
+
+        res.setHeader('Content-Type', 'image/png');
+        res.send(response.data);
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({ status: false });
+    }
+});
+
+module.exports = router;
