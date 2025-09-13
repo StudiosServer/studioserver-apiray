ca0ea31d7df053c5a3313553f416442183018451
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:11:25 -0500

    Add routes/anime/lolipc.js

---
+++ routes/anime/lolipc.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+router.get('/anime/lolipc', async (req, res) => {
+    try {
+        // URL de la imagen que deseas descargar
+        const imageUrl = 'https://www.loliapi.com/acg/pc/';
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+        // Guardar la imagen en el directorio especificado
+        const imagePath = path.join(__dirname, 'tempdata/lolipc.webp');
+        fs.writeFileSync(imagePath, response.data);
+
+        // Enviar la imagen como respuesta
+        res.sendFile(imagePath);
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({ status : false });
+    }
+});
+
+module.exports = router;
