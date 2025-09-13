bae7613708ebefbf2a72ed38dd570099217f3281
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:19:46 -0500

    Upload routes/random/duck.js

---
+++ routes/random/duck.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+router.get('/random/duck', async (req, res) => {
+    try {
+        // URL de la imagen que deseas descargar
+        const imageUrl = 'https://delirius-apiofc.vercel.app/random/duck';
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+        // Guardar la imagen en el directorio especificado
+        const imagePath = path.join(__dirname, 'tempdata/duck.png');
+        fs.writeFileSync(imagePath, response.data);
+
+        // Enviar la imagen como respuesta
+        res.sendFile(imagePath);
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({ status : false });
+    }
+});
+
+module.exports = router;
