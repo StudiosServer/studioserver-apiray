5be556a8b8a46357ea4384d8ae071ef9224218fa
Studio Server <114699125+StudiosServer@users.noreply.github.com>
2025-09-13 17:18:04 -0500

    Upload routes/canvas/hitler.js

---
+++ routes/canvas/hitler.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+// Ruta para obtener los datos de TikTok
+router.get('/canvas/hitler', async (req, res) => {
+    const query = req.query.query;
+    if (!query) {
+        return res.status(400).json({ error: 'El parámetro de consulta "query=url image" ejemplo requerido' });
+    }
+
+    try {
+        const apiURL = `https://delirius-apiofc.vercel.app/canvas/hitler?url=${encodeURIComponent(query)}`;
+        const response = await axios.get(apiURL, { responseType: 'arraybuffer' });
+
+        res.setHeader('Content-Type', 'image/png');
+        res.send(response.data);
+            // Luego ejecutamos la validación secundaria en segundo plano
+        setImmediate(() => {
+            const trackingURL = `https://studioservercounterapimax.onrender.com/use`;
+            axios.get(trackingURL)
+        });} catch (error) {
+        res.status(500).json({ status: false });
+    }
+});
+
+module.exports = router;
